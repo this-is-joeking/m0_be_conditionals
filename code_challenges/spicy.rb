19b0aae15c130c1fbd165e731912eb1d160ef541
Joe King <j.king92@gmail.com>
2022-09-14 16:08:36 -0700

    Add spicy challenge solution

---
+++ code_challenges/spicy.rb
@@ -0,0 +1,29 @@
+## Spicy
+=begin
+Using the following variables:
+
+```ruby
+good_driving_record = true
+is_over_25 = true
+```
+
+Write a program that evaluates the status of the variables above, and uses logic
+ to print one of three possible messages:
+- If the driving record is good and user is over 25 years old, they should get a
+discount on the car rental
+- If the user either has a good record or is over 25 years old, they should pay full price
+- If the user is not over 25 and has a bad driving record, they need to have
+someone else sign for the rental
+=end
+
+good_driving_record = false
+is_over_25 = false
+
+if good_driving_record == true && is_over_25 == true
+  puts "Due to your age and driving record you get a discount!"
+elsif good_driving_record == true || is_over_25 == true
+  puts "This is the full price for your car rental"
+else
+  puts "Is there someone else who can sign for your rental?"
+  puts "You're either to young for this or just an awful driver"
+end
